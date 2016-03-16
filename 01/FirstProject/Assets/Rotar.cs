ed0170ceaae4af0276f418d785b525f21b8e1bad
Alumno <Alumno>
2016-03-15 20:08:10 -0500

    First commit

---
+++ 01/FirstProject/Assets/Rotar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotar : MonoBehaviour { 
+	float t;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frames
+	void Update () {
+		t += 0.1f;
+		transform.Rotate (Mathf.Cos(t), Mathf.Sin(t), Mathf.Sin(t));
+	}
+}
